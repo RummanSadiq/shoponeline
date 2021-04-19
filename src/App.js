0e03d4fba250f686344ca04803ebf7a99c64116f
Rumman Sadiq <ummansadiq@gmail.com>
2021-04-19 16:43:07 +0500

    chatbot removed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardPage from './components/cardpage/cardpage';
 import Footer from './components/footer/footer';
 import NavbarHeader from './components/header/navheader/navbarheader';
 import TopHeader from './components/header/topheader/topheader';
+
 // import MainContainer from './components/mainbody/maincontainer';
 
 function App() {
