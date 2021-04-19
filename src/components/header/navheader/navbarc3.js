84aac51e2d9cd6c7bf9c4c7f76b3b9e368237cb6
Rumman Sadiq <ummansadiq@gmail.com>
2021-04-19 12:44:21 +0500

    modal for contact us is completed

--- src/components/header/navheader/navbarc3.js
+++ src/components/header/navheader/navbarc3.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'react-responsive-modal/styles.css';
+import {Modal} from 'react-responsive-modal'
+import { Formik, Field, Form } from 'formik';
 
 const NavbarC3 = () =>{
+  const [isOpen,setIsOpen] = useState(false);
+
+  const onOpen = () => setIsOpen(true);
+
+  const onClose = () => setIsOpen(false);
   return(
-    <>
-      Contact Us
-    </>
+    <div>
+      <div onClick={onOpen}>Contact Us</div>
+      <Modal open={isOpen} onClose={onClose} center>
+        <h2 className="modal-header">Contact Us</h2>
+        <Formik
+          initialValues={{name:'',email:'',subject:'',message:''}}
+          onSubmit={console.log('message received')}>
+          <Form  className="modal-grid">
+            <Field type='text' name='name' placeholder='Your Name' className="mg-component mg-componet1" />
+            <Field type='email' name='email' placeholder='Enter Email Address' className="mg-component mg-componet2" />
+            <Field type='text' name='subject' placeholder='Subject' className="mg-component mg-componet3" />
+            <Field type='text' name='message' placeholder='Leave Message' className="mg-component mg-componet4" />
+            <div className="mg-component mg-componet5"><button type="submit" className="m-send-meassage">Send Message</button></div>
+          </Form>
+        </Formik>
+      </Modal>
+    </div>
   )
 }
 
